4ca9dc9b58148b467aee2b1d7f13306fd649699f
Almira M Anderson <39601411+almiracma@users.noreply.github.com>
2022-01-20 15:02:07 -0800

    started from scratch

--- server.js
+++ server.js
@@ -1,42 +1,23 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
-const express = require("express");
+// Express to run server and routes
 
 // Start up an instance of app
-const app = express();
 
+/* Dependencies */
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
+//Here we are configuring express to use body-parser as middle-ware.
 // Cors for cross origin allowance
-const cors = require("cors");
-app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static("website"));
 
-// Setup Server
-const port = 3000;
+// Spin up the server
+// Callback to debug
 
-const server = app.listen(port, () => {
-  console.log(`running localhost: ${port}`);
-});
+// Initialize all route with a callback function
 
-app.get("/data", function (req, res) {
-  res.send(projectData);
-});
+// Callback function to complete GET '/all'
 
-app.post("/newData", function (req, res) {
-  let data = req.body;
-
-  projectData["date"] = data.date;
-  projectData["temp"] = data.temp;
-  projectData["feelings"] = data.feelings;
-
-  res.send(projectData);
-});
+// Post Route
--- website/app.js
+++ website/app.js
@@ -2,51 +2,16 @@
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
 
+// Personal API Key for OpenWeatherMap API
 
-const baseUrl = "http://api.openweathermap.org/data/2.5/weather?zip=";
-const apiKey = ",us&appid=e32478fe18ea0e7eab9385983090fb94";
-let zip;
+// Event listener to add function to existing HTML DOM element
 
-document.getElementById('generate').addEventListener('click', performAction);
+/* Function called by event listener */
 
-function performAction(e) {
-    const feelings = document.getElementById('feelings').value;
-    zip = document.getElementById('zip').value;
-    console.log(newDate);
-    getWeather()
-    .then(function (data) {
-        postData('/newData', {temperature: data.main.temp, date: data.date, userResponse: data.userResponse})
-    })
-}
+/* Function to GET Web API Data*/
 
-const getWeather = async () => {
-    const res = await fetch(baseUrl+zip+apiKey)
-    try {
-        const data = await res.json();
-        console.log(data);
-    } catch(error) {
-        console.log("error", error);
-    }
-}
+/* Function to POST data */
 
-const postData = async ( url = '', data = {})=>{
-
-    const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    credentials: 'same-origin', 
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data), // body data type must match "Content-Type" header        
-  });
-
-    try {
-      const newData = await response.json();
-             return newData
-    }catch(error) {
-    console.log("error", error);
-    // appropriately handle the error
-    }
-}
+/* Function to GET Project Data */
